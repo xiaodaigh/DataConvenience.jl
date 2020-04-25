e9579de794d5113f790983ca17cb63b23c92824f
xiaodaigh <zhuojia.dai@gmail.com>
2020-04-25 17:44:54 +1000

    fixed bugs

--- src/read-csv-in-chunks.jl
+++ src/read-csv-in-chunks.jl
@@ -2,7 +2,7 @@ export CsvChunkIterator
 
 
 using CSV
-using DataFrames: DataFrame
+using DataFrames: DataFrame, names
 
 import Base: iterate, length, IteratorSize
 using Base.Iterators
@@ -15,10 +15,12 @@ Define a Chunking iterator on CSV file
 mutable struct CsvChunkIterator
     file::IOStream
     step::Int
+    first_read::Bool # reading  the first time
+    column_headers::Vector{Symbol}
     csv_rows_params
 
     CsvChunkIterator(path::String; csv_rows_params...) = begin
-         new(open(path, "r"), 2^30, csv_rows_params)
+         new(open(path, "r"), 2^30, true, Symbol[], csv_rows_params)
     end
 end
 
@@ -29,16 +31,36 @@ Base.iterate(chunk_iterator::CsvChunkIterator) = begin
     if isnothing(last_newline_pos) & (length(bytes_read) == 0)
         close(chunk_iterator.file)
         return nothing
-    elseif length(bytes_read) == 0
-        # do nothing
     end
-    df =
-        CSV.read(
-            IOBuffer(
-                @view bytes_read[1:last_newline_pos]
-            );
-            chunk_iterator.csv_rows_params...
-        )
+    if chunk_iterator.first_read
+        df =
+            CSV.read(
+                IOBuffer(
+                    @view bytes_read[1:last_newline_pos]
+                );
+                chunk_iterator.csv_rows_params...
+            )
+
+        chunk_iterator.column_headers = names(df)
+
+        # make the second read not read header
+        chunk_iterator.first_read = false
+
+        # removes header options from table
+        c = chunk_iterator.csv_rows_params
+        d = Dict(c for c in zip(keys(c), values(c)))
+        delete!(d, :header)
+        chunk_iterator.csv_rows_params = (; d...)
+    else
+        df =
+            CSV.read(
+                IOBuffer(
+                    @view bytes_read[1:last_newline_pos]
+                );
+                header=chunk_iterator.column_headers,
+                chunk_iterator.csv_rows_params...
+            )
+    end
     return df, nothing
 end
 
