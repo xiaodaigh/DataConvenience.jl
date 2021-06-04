50921153b91d6024eea49fb302f3a94d7d1cb553
evalparse <zhuojia.dai@gmail.com>
2021-06-04 22:13:57 +1000

    Create shortstringify.jl

---
+++ src/shortstringify.jl
@@ -0,0 +1,16 @@
+# Original code courtesy of Nils Gudat
+
+# Functions to turn String columns into ShortStrings
+function short_stringify(x::AbstractVector)
+    y = ShortString("a"^maximum(length.(skipmissing(x))))
+    return passmissing(typeof(y)).(x)
+end
+
+function short_stringify!(df::DataFrame)
+    cols = unique([names(df, String); names(df, Union{Missing, String})])
+    for c ∈ cols
+        df[!, c] = PooledArray(short_stringify(df[!, c]))
+    end
+    return df
+end
+  
